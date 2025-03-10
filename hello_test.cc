3d68abed19e6e5fe76a9d0da71f3f657468b76af
Patrik <Muravski.patrik@gmail.com>
2025-03-10 11:50:37 +0200

    input test valid

--- hello_test.cc
+++ hello_test.cc
@@ -1,11 +1,22 @@
 #include <gtest/gtest.h>
+#include "input.h"
+
+
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("world", "world");
+TEST(InputTest, EmptyExpression) {
   // Expect equality.
-  EXPECT_EQ(5 * 6, 42);
+  //EXPECT_EQ(5 * 6, 42);
+  std::string INPUT1 = "1.000000+2*9-8/3*(2-1)-32.4";
+  std::string EXPECT1 = "( ( ( 1.000000 + ( 2 * 9 ) ) - ( ( 8 / 3 ) * ( 2 - 1 ) ) ) - 32.4 )";
+
+
+  Input input1(INPUT1);
+  std::string expression = input1.result();
+
+  // Expect two strings to be equal.
+  EXPECT_EQ(expression, EXPECT1);
+
 
   system("pause");
 }
