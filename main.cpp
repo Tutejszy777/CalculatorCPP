3a4db473d294542238cb47b1218cb3999803d779
Patrik <Muravski.patrik@gmail.com>
2025-03-11 00:40:57 +0200

    added validation for user input

--- main.cpp
+++ main.cpp
@@ -3,21 +3,70 @@
 #include "tree.h"
 
 
+// INCOMPLETE VALIDATION for most cases as it's not mandatory
+bool validationOfInput(std::string a)
+{
+    std::stack<char> par;
+    std::string last = "";
+    for(int i = 0; i < a.length(); i++){
+        if(isspace(a[i])) continue;
+
+        if( isdigit(a[i]) || a[i] == '.')
+        {
+            while(isdigit(a[i]) || a[i] == '.'){
+                i++;
+            }
+            i--;
+            if(last == "digit" || last == "x") return false;
+            last = "digit";
+        }
+        else if( a[i] == '+' || a[i] == '-' || a[i] == '*' || a[i] == '/')
+        {
+            if(last == "operator") return false;
+            last = "operator";
+        }
+        else if(isalpha(a[i]))
+        {
+            if(last == "digit" || last == "x") return false;
+            last = "x";
+        }
+        else if (a[i] == '(')
+        {
+            if(last == "digit" || last == "x" || last == "operator")
+            par.push(a[i]);
+        }
+        else if (a[i] == ')')
+        {
+            if(!par.empty()){
+                par.pop();
+            }
+            else return false;
+        }
+        else std::cout << "unhandled case appeared\n\n";
+    }
+
+    if(!par.empty()) return false;
+
+    return true;
+}
+
 int main(int, char**){
-    
-    std::cout << "Use Proper Format (4*x) NOT  (4x). REMEMBER TO USE MULTIPLICATION SIGN. \n";
+    std::cout << "Example of valid input:  4*x + 5*( 2-1)-3\n";
     std::cout << "Enter a line of simple arithmetic or algebra expression: ";
 
     std::string expression;
-    //(VALIDATION NEEDED but it wasn't a requirment)
     std::getline(std::cin, expression); // user enters the string 
 
-    Input input(expression); // passes the string to make into proper format for building a tree
-    expression = input.result();
+    if(validationOfInput(expression))
+    {
+        Input input(expression); // passes the string to make into proper format for building a tree
+        expression = input.result();
 
-    //(VALIDATION IS NOT NEEDED IN THIS CASE BUT NICE TO HAVE)
-    ExpressionTree tree(expression); // building a tree
-    std::cout << "\n\n" << tree.Expression() << " = " << tree.Evaluate() << "\n\n" << std::endl; // evaluating
+        //(VALIDATION IS NOT NEEDED IN THIS CASE BUT NICE TO HAVE)
+        ExpressionTree tree(expression); // building a tree
+        std::cout << "\n\n" << tree.Expression() << " = " << tree.Evaluate() << "\n\n" << std::endl; // evaluating
+    }
+    else std::cout << "INVALID INPUT\n\n";
 
     system("pause");
     return 0;
